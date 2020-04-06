e368da7fe655c03010f223e3ad064380199e2f40
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-04-06 06:41:33 +0100

    another commiyt

--- superglobals/_server.php
+++ superglobals/_server.php
@@ -2,5 +2,11 @@
 
     echo $_SERVER['PHP_SELF'];
     echo "<br/>";
-    echo 
+    echo $_SERVER['SERVER_NAME'] . "<br/>";
+    echo $_SERVER['SERVER_ADDR'] . "<br/>";
+
+    echo $_SERVER['SERVER_PROTOCOL'] . "<br/>";
+    echo $_SERVER['GATEWAY_INTERFACE'] . "<br/>";
+    echo $_SERVER['REQUEST_METHOD'] . "<br/>";
+    echo $_SERVER['SERVER_SOFTWARE'] . "<br/>";
 ?>
